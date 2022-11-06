383e6d4ee0b46e101440d7a43818713afa75a8fd
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-11-06 15:46:56 -0800

    Changed order of commands in menu

--- src/triggers.js
+++ src/triggers.js
@@ -2,13 +2,13 @@
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Ride Lead Coordinators')
-    .addItem('Link Selected Route URLs', linkRouteURLs.name)
-    .addItem('Import Selected Routes', importSelectedRoutes.name)
-    .addSeparator()
     .addItem('Schedule Selected Rides', scheduleSelectedRides.name)
     .addItem('Update Selected Rides', updateSelectedRides.name)
     .addItem('Cancel Selected Rides', cancelSelectedRides.name)
     .addItem('Unschedule Selected Rides', unscheduleSelectedRides.name)
+    .addSeparator()
+    .addItem('Import Selected Routes', importSelectedRoutes.name)
+    .addItem('Link Selected Route URLs', linkRouteURLs.name)
     .addSeparator()
     .addItem('Clear User Credentials', clearCredentials.name)
     .addToUi();
