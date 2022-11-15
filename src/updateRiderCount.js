36bcdb38dd99007693989ef1f7e0bb56014a3914
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-11-14 16:19:58 -0800

    Stopped ride leader being deleted when updating count

--- src/updateRiderCount.js
+++ src/updateRiderCount.js
@@ -7,6 +7,34 @@ function duration(msg, start, end){
   return `${msg} duration: ${(end-start)/1000}S`;
 }
 
+/**
+     * Fixup the organizers (ie. ride leaders) in the given event. 
+     * @param {object} rwgps - the rwgps object used to talk to rwgps
+     * @param {object} event - the event object to be fixed
+     */
+ function fixup_organizers(rwgps, event) {
+  const organizers = event.organizer_names.map(name => rwgps.lookupOrganizer(A_TEMPLATE, name)).reduce((p, o) => {
+    if (o.id !== RIDE_LEADER_TBD_ID) {
+      p.known.push(o)
+    } else {
+      p.unknown.push(o)
+    };
+    return p;
+  },
+    { known: [], unknown: [] });
+  event.organizer_tokens = organizers.known.map(o => o.id + "");
+  const names = organizers.known.map(o => o.text);
+
+  // Only if there are no known organizers will the defaults be used
+  if (!organizers.known.length) {
+    event.organizer_tokens.push(RIDE_LEADER_TBD_ID + "");
+    names.push(RIDE_LEADER_TBD_NAME);
+  }
+
+  event.desc = `Ride Leader${names.length > 1 ? "s" : ""}: ${names.join(', ')}
+
+${event.desc}`;
+}
 function updateRiderCountWithCreds(rows, rwgps) {
   let start = new Date().getTime();
   rows = Schedule.getYoungerRows(new Date());
@@ -16,6 +44,7 @@ function updateRiderCountWithCreds(rows, rwgps) {
   rows.filter(row => !rowCheck.unmanagedRide(row) && rowCheck.alreadyScheduled(row)).forEach(row => {
     start = new Date().getTime();
     const event = new Event(row)
+    fixup_organizers(rwgps, event);
     const numRideLeaders = !row.RideLeader ? 0 : row.RideLeader.split(',').map(rl => rl.trim()).filter(rl => rl).length;
     event.updateRiderCount(rwgps.getRSVPCount(event.getRideLinkURL()) + numRideLeaders);
     rwgps.edit_event(event.getRideLinkURL(), event);
