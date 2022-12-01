24108595da0e334f119c723743d49651d2c3f3af
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-12-01 14:25:00 -0800

    Implemented parallel mechanism for updating rider count

--- src/updateRiderCount.js
+++ src/updateRiderCount.js
@@ -7,8 +7,8 @@ function updateRiderCount() {
   do_action(form);
 }
 
-function duration(msg, start, end){
-  return `${msg} duration: ${(end-start)/1000}S`;
+function duration(msg, start, end) {
+  return `${msg} duration: ${(end - start) / 1000}S`;
 }
 
 function updateRiderCountWithCreds(rows, rwgps) {
@@ -17,13 +17,22 @@ function updateRiderCountWithCreds(rows, rwgps) {
   let end = new Date().getTime();
   console.log(duration("getYoungerRows", start, end));
 
-  rows.filter(row => rowCheck.alreadyScheduled(row)).forEach(row => {
-    start = new Date().getTime();
-    const event = EventFactory.fromRwgpsEvent(rwgps.get_event(row.RideURL));
-    event.updateRiderCount(rwgps.getRSVPCount(row.RideURL) + row.RideLeaders.length);
-    rwgps.edit_event(row.RideURL, event);
+  start = new Date().getTime();
+  const scheduledRows = rows.filter(row => rowCheck.alreadyScheduled(row))
+  const scheduledRowURLs = scheduledRows.map(row => row.RideURL);
+  const rwgpsEvents = rwgps.get_events(scheduledRowURLs);
+  const scheduledEvents = rwgpsEvents.map(e => EventFactory.fromRwgpsEvent(e));
+  const rsvpCounts = rwgps.getRSVPCounts(scheduledRowURLs);
+  //updatedEvents is a boolean array, where true values mean that the count has changed.
+  const updatedEvents = scheduledEvents.map((event, i) => event.updateRiderCount(rsvpCounts[i] + scheduledRows[i].RideLeaders.length));
+  // We only want to edit events which have changed.
+  const edits = updatedEvents.reduce((p, e, i) => { if (e) { p.push({ row: scheduledRows[i], event: scheduledEvents[i] }) }; return p; }, [])
+  rwgps.edit_events(edits.map(({ row, event }) => { return { url: row.RideURL, event } }));
+  edits.forEach(({ row, event }) => {
     row.setRideLink(event.name, row.RideURL);
-    end = new Date().getTime();
-    console.log(duration(`row ${row.rowNum}`, start, end));
   })
+  const updatedRows = edits.map(({row, event}) => row.rowNum);
+  end = new Date().getTime();
+  console.log(duration(`row processing (${scheduledRows.length} rows, ${updatedRows.length} updated)`, start, end));
+  console.log(`rows updated: ${updatedRows.join(', ')}`);
 }
