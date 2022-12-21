4cfe12300377dfc6dbe6d2b2fc8166e1ac807f74
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-12-21 06:27:05 -0800

    Changed structure of FormHandling to make testable

--- src/FormHandling.js
+++ src/FormHandling.js
@@ -4,15 +4,44 @@
 
 
 const FormHandling = function () {
+  function _scheduleRide(event) {
+    console.log(event.namedValues);
+    function createRowData(event) {
+      const nv = event.namedValues;
+      const rowData = {}
+      rowData.StartDate = nv[`${FormSheet.RIDEDATECOLUMNNAME}`][0];
+      rowData.Group = nv[`${FormSheet.GROUPCOLUMNNAME}`][0];
+      rowData.StartTime = nv[`${FormSheet.STARTTIMECOLUMNNAME}`][0];
+      rowData.RouteURL = nv[`${FormSheet.ROUTEURLCOLUMNNAME}`][0];
+      rowData.RouteName = nv[`${FormSheet.ROUTEURLCOLUMNNAME}`][0];
+      rowData.RideLeaders = nv[`${FormSheet.FIRSTNAMECOLUMNNAME}`] + "  " + nv[`${FormSheet.LASTNAMECOLUMNNAME}`][0];
+      rowData.Email = nv[`${FormSheet.EMAILADDRESSCOLUMNNAME}`][0];
+      return rowData;
+    }
+    const rowData = createRowData(event);
+    console.log(rowData);
+    const result = {}
+    const rwgps = new RWGPS(new RWGPSService(credentials.email, credentials.password));
+    // eval_rows([rowData], rwgps, [rowCheck.badRoute, rowCheck.noRideLeader], []);
+    // if (rowData.errors && rowData.errors.length) {
+    //   result.errors = rowData.errors;
+    //   return result;
+    // }
+
+    const lastRow = Schedule.appendRow(rowData);
+    RideManager.scheduleRows([lastRow], rwgps);
+    result.row = lastRow;
+    return result;
+  }
+  // A resubmission is when the form range contains a reference to a row.
+  function _reSubmission(event) {
+    return Form.getReferenceCell(event.range)
+  }
   return {
-    // A resubmission is when the form range contains a reference to a row.
-    reSubmission: (event) => {
-      return Form.getReferenceCell(event.range)
-    },
     // docs for the event: https://developers.google.com/apps-script/guides/triggers/events
     processEvent: function (event) {
-      if (!this.reSubmission(event)) {
-        const result = this.scheduleRide(event);
+      if (_reSubmission(event)) {
+        const result = _scheduleRide(event);
         Form.setReferenceCell(event.range, `='${RideSheet.NAME}'!A${result.row.rowNum}`)
         // const email = composeScheduleEmailBody(result);
         // sendEmail(event[FormSheet.EMAILADDRESSCOLUMNNAME], email);
@@ -38,52 +67,29 @@ const FormHandling = function () {
       }
       Schedule.save();
     },
-    scheduleRide: (event) => {
-      console.log(event.namedValues);
-      function createRowData(event) {
-        const nv = event.namedValues;
-        const rowData = {}
-        rowData.StartDate = nv[`${FormSheet.RIDEDATECOLUMNNAME}`][0];
-        rowData.Group = nv[`${FormSheet.GROUPCOLUMNNAME}`][0];
-        rowData.StartTime = nv[`${FormSheet.STARTTIMECOLUMNNAME}`][0];
-        rowData.RouteURL = nv[`${FormSheet.ROUTEURLCOLUMNNAME}`][0];
-        rowData.RouteName = nv[`${FormSheet.ROUTEURLCOLUMNNAME}`][0];
-        rowData.RideLeaders = nv[`${FormSheet.FIRSTNAMECOLUMNNAME}`] + "  " + nv[`${FormSheet.LASTNAMECOLUMNNAME}`][0];
-        rowData.Email = nv[`${FormSheet.EMAILADDRESSCOLUMNNAME}`][0];
-        return rowData;
-      }
-      const rowData = createRowData(event);
-      console.log(rowData);
-      const result = {}
-      const rwgps = new RWGPS(new RWGPSService(credentials.email, credentials.password));
-      // eval_rows([rowData], rwgps, [rowCheck.badRoute, rowCheck.noRideLeader], []);
-      // if (rowData.errors && rowData.errors.length) {
-      //   result.errors = rowData.errors;
-      //   return result;
-      // }
+    tests: {
+      testGoodRide: function ()  {
+        let namedValues = {}
+        namedValues[`${FormSheet.RIDEDATECOLUMNNAME}`] = ["12/31/2024"];
+        namedValues[`${FormSheet.GROUPCOLUMNNAME}`] = ["A"];
+        namedValues[`${FormSheet.STARTTIMECOLUMNNAME}`] = ["10:00 AM"];
+        namedValues[`${FormSheet.ROUTEURLCOLUMNNAME}`] = ["https://ridewithgps.com/routes/30674325"];
+        namedValues[`${FormSheet.FIRSTNAMECOLUMNNAME}`] = ["Toby"];
+        namedValues[`${FormSheet.LASTNAMECOLUMNNAME}`] = ["Ferguson"];
+        namedValues[`${FormSheet.EMAILADDRESSCOLUMNNAME}`] = ["toby.h.ferguson@icloud.com"];
 
-      const lastRow = Schedule.appendRow(rowData);
-      RideManager.scheduleRows([lastRow], rwgps);
-      result.row = lastRow;
-      return result;
+        _scheduleRide({ namedValues: namedValues });
+        Schedule.save();
+      }
     }
   }
 }();
 
 
 function testGoodRide() {
-  let namedValues = {}
-  namedValues[`${FormSheet.RIDEDATECOLUMNNAME}`] = ["12/31/2024"];
-  namedValues[`${FormSheet.GROUPCOLUMNNAME}`] = ["A"];
-  namedValues[`${FormSheet.STARTTIMECOLUMNNAME}`] = ["10:00 AM"];
-  namedValues[`${FormSheet.ROUTEURLCOLUMNNAME}`] = ["https://ridewithgps.com/routes/30674325"];
-  namedValues[`${FormSheet.FIRSTNAMECOLUMNNAME}`] = ["Toby"];
-  namedValues[`${FormSheet.LASTNAMECOLUMNNAME}`] = ["Ferguson"];
-  namedValues[`${FormSheet.EMAILADDRESSCOLUMNNAME}`] = ["toby.h.ferguson@icloud.com"];
-
-  FormHandling.scheduleRide({ namedValues: namedValues });
-  Schedule.save();
+  FormHandling.tests.testGoodRide();
 }
+
 
 function testBadRide() {
   let rideData = {
