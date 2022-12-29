cbed106772f2db08173b9ca8c4cb326703fd4497
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-12-29 09:30:10 -0800

    Eliminated change handling as update mechanism

--- src/FormHandling.js
+++ src/FormHandling.js
@@ -14,71 +14,28 @@ const FormHandling = function () {
   }
 
   /**
-   * Update the given row from the selection of namedValues.
-   * 
-   * Although the function will always complete if there are any errors then the underlying ride 
-   * won't have been updated. 
-   * @param {Row} row The row to be updated
-   * @param {Object} namedValues The NV object from the Form Submit event
-   * @param {RWGPS} rwgp The rwgps connection to update the actual event
-   * @param {Object} result the result object to be updated
-   * @return {boolean} true iff the row has been updated
-   */
-  function _updateRow(row, namedValues, rwgps, result) {
-    console.log(`FormHandling - updating row`)
-    console.log(namedValues);
-    let updated = false;
-    let v = namedValues[`${FormSheet.RIDEDATECOLUMNNAME}`][0];
-    if (v) { row.StartDate = dates.convert(v); updated = true; }
-    v = namedValues[`${FormSheet.STARTTIMECOLUMNNAME}`][0];
-    if (v) { row.StartTime = dates.convert(`12/30/1899 ${v}`); updated = true; }
-    v = namedValues[`${FormSheet.GROUPCOLUMNNAME}`][0];
-    if (v) { row.Group = v; updated = true; }
-    v = namedValues[`${FormSheet.ROUTEURLCOLUMNNAME}`][0];
-    if (v) { row.setRouteLink(v, v); row.linkRouteURL(); updated = true; };
-
-
-    if (namedValues[`${FormSheet.FIRSTNAMECOLUMNNAME}`][0] || namedValues[`${FormSheet.LASTNAMECOLUMNNAME}`][0]) {
-      let oldRideLeader = row.RideLeaders[0];
-      let newRideLeader = oldRideLeader;
-
-      const [oldFirst, oldLast] = oldRideLeader.split(' ');
-      let newFirst = namedValues[`${FormSheet.FIRSTNAMECOLUMNNAME}`][0].trim();
-      let newLast = namedValues[`${FormSheet.LASTNAMECOLUMNNAME}`][0].trim();
-      newFirst = (newFirst && newFirst !== oldFirst) ? newFirst : oldFirst;
-      newLast = (newLast && newLast !== oldLast) ? newLast : oldLast;
-      newRideLeader = `${newFirst} ${newLast}`;
-      if (newRideLeader !== oldRideLeader) {
-        row.RideLeaders = [newRideLeader];
-        updated = true;
-      }
-    }
-    return updated;
-  }
-
-  /**
    * Using the given row, update the corresponding ride
    * @param {Row} row Row from which to update the ride
    * @param {RWGPS} rwgps RWGPS object to connect to ride
    * @param {Result} result result object to collect result of underlying operations
    * @returns {Result} result object
    */
-  function updateRide(row, rwgps, result) {
+  function _updateRide(row, rwgps, result) {
     row.linkRouteURL();
     RideManager.updateRows([row], rwgps);
     result.row = row;
     return result;
   }
 
-  function copyFormDataIntoRow(event, row) {
+  function _copyFormDataIntoRow(event, row) {
     const rng = event.range;
     row.StartDate = Form.getRideDate(rng);
     row.Group = Form.getGroup(rng);
     row.StartTime = Form.getStartTime(rng);
-    row.RouteURL = Form.getRouteURL(rng);
-    row.RouteName = Form.getRouteURL(rng);
-    row.RideLeaders = Form.getFirstName(rng) + " " + Form.getLastName(rng);
+    row.setRouteLink(Form.getRouteURL(rng), Form.getRouteURL(rng));
+    row.RideLeaders = [Form.getFirstName(rng) + " " + Form.getLastName(rng)];
     row.Email = Form.getEmail(rng);
+    Schedule.save();
     return row;
   }
 
@@ -92,8 +49,7 @@ const FormHandling = function () {
     //   return result;
     // }
     const newRow = {};
-    copyFormDataIntoRow(event, newRow);
-    console.log(newRow);
+    _copyFormDataIntoRow(event, newRow);
     const lastRow = Schedule.appendRow(newRow);
     RideManager.scheduleRows([lastRow], rwgps);
     result.row = lastRow;
@@ -112,9 +68,8 @@ const FormHandling = function () {
         // console.log(result);
       } else {
         const row = _getRow(event.range);
-        if (_updateRow(row, event.namedValues, result)) {
-          updateRide(row, rwgps, result);
-        };
+        _copyFormDataIntoRow(event, row);
+        _updateRide(row, rwgps, result);
         // console.log(result);
         // Need to handle cancel/reinstate
       }
