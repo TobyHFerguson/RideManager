83629046da4d5610d68835c4e674f0886146c960
Toby Ferguson <toby.h.ferguson@icloud.com>
2022-12-31 14:33:42 -0800

    Tidied up email responses

--- src/Email.js
+++ src/Email.js
@@ -6,7 +6,12 @@ const Email = function () {
         help(row, email) {
             const body = this.formatHelpEmail(row, email);
             this.sendEmail('Help needed', body, "rides@santacruzcycling.org");
-            this.sendEmail('Help request noted', `${email}, you requested help. We've notified the Ride group and you should hear from them soon. You can contact them directly at 'rides@santacruzcycling.org'`, email)
+            this.sendEmail('Help request noted', `${email},<p> you requested help. 
+            <p>
+            We've notified the Ride group and you should hear from them soon. 
+            <p>
+            You can contact them directly at rides@santacruzcycling.org`, 
+            email)
         },
         rideDeleted(row, email) {
             const body = this.formatDeletionEmail(row);
@@ -23,7 +28,8 @@ const Email = function () {
         sendEmail(subject, body, recipient) {
             console.log(recipient);
             console.log(body);
-            GmailApp.sendEmail(`${recipient}`, `SCCCC: ${subject}`, '', { htmlBody: body });
+            body =  `${body}<p>SCCCC Ride Director`
+            GmailApp.sendEmail(`${recipient}`, `SCCCC: ${subject}`, '', { htmlBody:body  });
         },
         rideSubmitted(row, email) {
             const body = this.formatSubmissionEmail(row)
@@ -40,25 +46,22 @@ const Email = function () {
             return html;
         },
         formatDeletionEmail(row) {
-            var html = `${row.RideLeaders[0]}, \n `
+            var html = `${row.RideLeaders[0]},<p> `
             html += `The ride ${row.RideName} has been deleted and is no longer visible on the schedule.`
-            html += '<p>However the ride can still be managed using the Google Form you originally used to create the ride'
-            html += '<p>SCCCC Ride Organization<p>';
+            html += '<p>However the ride can still be managed using the Google Form you originally used to create the ride.'
             return html;
         },
         formatCancellationEmail(row) {
             var html = `${row.RideLeaders[0]}, \n `
             html += `The ride '${makeLink(row.RideURL, row.RideName.replace('CANCELLED: ',''))}' has been cancelled.`
             html += '<p>However the ride can still be managed using the Google Form you originally used to create the ride'
-            html += '<p>SCCCC Ride Organization<p>';
             return html;
         },
         formatResubmissionEmail(row) {
-            var html = `${row.RideLeaders[0]}, \n `
-            html += `The ride ${row.RideName} has been resubmitted`
-            html += '<p>However the ride can still be managed using the Google Form you originally used to create the ride'
+            var html = `${row.RideLeaders[0]}, <p> `
+            html += `you requested modifications to the ride ${makeLink(row.RideURL, row.RideName)}.<p>`
             if (row.errors && row.errors.length) {
-                html += `Unfortunately the ride had the following errors and could not be scheduled. Please correct these errors, by
+                html += `Unfortunately the ride had the following errors and was not updated. Please correct these errors by
                 editing and then resubmitting the form`
                 html += '<ul>\n'
                 for (const e of row.errors) {
@@ -66,7 +69,7 @@ const Email = function () {
                 }
                 html += '</ul>\n';
             } else {
-                html += `<p>The ride has now been scheduled and is live on the club calendar and in RideWithGPS. `
+                html += `The ride has now been scheduled and is live on the club calendar and in RideWithGPS. `
                 if (row.warnings && row.warnings.length) {
                     html += `<p>The ride did have some warnings, which you should attend to and fix by editing the form and resubmitting the ride:`
                     html += '<ul>\n'
@@ -76,8 +79,7 @@ const Email = function () {
                     html += '</ul>\n';
                 }
                 html += `<p>Please use the following link in any emails about the ride:<p>\n`
-                html += `<a href="${row.RideURL}">${row.RideName}</a>`
-                html += '<p>SCCCC Ride Organization<p>';
+                html += makeLink(row.RideURL,row.RideName);
             }
             return html;
         },
@@ -96,7 +98,7 @@ const Email = function () {
             } else {
                 html += `<p>The ride has now been scheduled and is live on the club calendar and in RideWithGPS. `
                 if (row.warnings && row.warnings.length) {
-                    html += `<p>The ride did have some warnings, which you should attend to and fix by editing the form and resubmitting the ride:`
+                    html += `<p>The ride did however have some warnings, which you should attend to and fix by editing the form and resubmitting the ride:`
                     html += '<ul>\n'
                     for (const e of row.warnings) {
                         html += `<li>${e}</li>\n`;
@@ -104,8 +106,7 @@ const Email = function () {
                     html += '</ul>\n';
                 }
                 html += `<p>Please use the following link in any emails about the ride:<p>\n`
-                html += `<a href="${row.RideURL}">${row.RideName}</a>`
-                html += '<p>SCCCC Ride Organization<p>';
+                html += makeLink(row.RideURL,row.RideName);
             }
             return html;
         }
