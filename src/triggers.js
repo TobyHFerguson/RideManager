5fe1374d9b7e29f40a3b3856e737143d7da5e2f6
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-01-05 09:06:03 -0800

    Fixed Issue #14 - automated the linking of route urls

--- src/triggers.js
+++ src/triggers.js
@@ -14,6 +14,54 @@ function onOpen() {
     .addToUi();
 }
 
+function onEdit(event) {
+  console.log('onEdit()')
+  console.log(`Editing: ${event.range.getA1Notation()}`)
+  
+  if (event.range.getRow() > 1) {
+    switch (event.range.getColumn() - 1) { // convert from Spreadsheet numbering (1 ...) to object numbering (0 ...)
+      case Schedule.getColumnIndex(Globals.ROUTECOLUMNNAME):
+        _editRouteColumn(event);
+        break;
+    }
+  }
+}
 
+// The value could be an rtv.
+  // if the url & text are defined and equal then this rtv has been auto-linked and we need to look up the url
+  // if the url & text are defined and unequal then linking has occurred. Return false
+  // if the text is defined it contains an url (that's why we have an RTV!). Return it.
+  // otherwise return whatever the url has
+
+  function _rtvNeedingFetch(rtv) {
+    console.log(`rtv.getLinkUrl(): ${rtv.getLinkUrl()}, rtv.getText(): ${rtv.getText()}`)
+    if (!rtv) return false;
+    let result;
+    const url = rtv.getLinkUrl();
+    const text = rtv.getText();
+    result = (url && text) ? ((url == text) ? url : false) : text ? text : url
+
+    console.log(`result: ${result}`)
+    return result
+  }
+
+function _editRouteColumn(event) {
+  let url = event.value || _rtvNeedingFetch(event.range.getRichTextValue())
+  if (url) {
+    const options = {
+      headers: {
+        Accept: "application/json" // Return json, not html
+      },
+    }
+    try {
+      const response = UrlFetchApp.fetch(url, options)
+      const route = JSON.parse(response.getContentText());
+      const name = `${(route.user_id !== Globals.SCCCC_USER_ID) ? Globals.FOREIGN_PREFIX : ''}` + route.name;
+      event.range.setValue(`=hyperlink("${url}", "${name}")`)
+    } catch (e) {
+      console.log(`onEdit._editRouteColumn() - fetching ${url} got exception: ${e}`)
+    }
+  }
+}
 
 
