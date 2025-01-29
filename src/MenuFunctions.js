01d9d4503f7165181248143bf8b0a3b7d8a0c4ae
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-01-29 09:23:27 -0800

    Added caching of the Groups data

--- src/MenuFunctions.js
+++ src/MenuFunctions.js
@@ -67,6 +67,39 @@ const MenuFunctions = (() => {
     finally {
       Schedule.save();
     }
+
+    function getGroups() {
+      const cache = CacheService.getScriptCache();
+      let cachedGroups = cache.get('groups');
+      if (cachedGroups) {
+        return JSON.parse(cachedGroups);
+      } else {
+        let groups = getGroupsFromSheet();
+        cache.put('groups', JSON.stringify(groups), 21600); // Cache for 6 hours
+        return groups;
+      }
+
+      function getGroupsFromSheet() {
+        const fiddler = bmPreFiddler.PreFiddler().getFiddler({
+          sheetName: 'Groups',
+          createIfMissing: false
+        });
+        let groups = fiddler.getData();
+        // groups = [ {"Group": "A", "Template": ..., "MIN_LENGTH": ...}]
+        groups = groups.reduce((acc, curr) => {
+          const { Group, ...rest } = curr;
+          acc.push({ [`${Group}`]: rest });
+          return acc;
+        }, []);
+        // groups = [ {"A": {"TEMPLATE": ..., "MIN_LENGTH": ...}}, {"B": {{"TEMPLATE": ..., "MIN_LENGTH": ...}}}]
+        const g2 = groups.reduce((acc, curr) => {
+          const [key, value] = Object.entries(curr)[0];
+          acc[key] = value;
+          return acc;
+        }, {});
+        return g2;
+      }
+    }
   }
   return Object.freeze({
     cancelSelectedRides() {
