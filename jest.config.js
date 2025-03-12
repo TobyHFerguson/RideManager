ce498e9498402ef45aeadd7deccece3756833c9a
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-03-12 09:39:58 -0700

    Added Dates test to jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,17 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/__tests__/**/*.test.js'],
+  roots: ['<rootDir>/test', '<rootDir>/submodules/Dates/test'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: ['src/**/*.js', 'submodules/Dates/src/**/*.js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
