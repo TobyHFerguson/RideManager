dd013bda2596fe0806a1df39ecc5af9c6c52ee72
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-22 07:43:53 -0700

    Handled returning an AggregateError

--- src/MenuFunctions.js
+++ src/MenuFunctions.js
@@ -22,18 +22,27 @@ const MenuFunctions = (() => {
     const rwgpsService = RWGPSLib.newRWGPSService(Credentials.username, Credentials.password, globals);
     const rwgps = RWGPSLib.newRWGPS(rwgpsService);
     let rows = Schedule.getSelectedRows();
-    console.info('User %s', Session.getActiveUser());
+    const rowNumbers = rows.map(row => row.rowNum).join(", ");
+    console.info(`'User ${Session.getActiveUser()} executing ${command.name} on row numbers: ${rowNumbers}`);
     try {
       command(rows, rwgps, force);
     } catch (e) {
-      console.error(e);
+      if (e instanceof AggregateError) {
+        for (const error of e.errors) {
+          console.error(error.stack);
+        }
+      } else if (e instanceof Error) {
+        console.error(e.stack);
+      } else {
+        console.error("Unknown error: ", e);
+      }
       throw (e);
     }
     finally {
       Schedule.save();
     }
 
-    
+
   }
   return Object.freeze({
     cancelSelectedRides(force = false) {
